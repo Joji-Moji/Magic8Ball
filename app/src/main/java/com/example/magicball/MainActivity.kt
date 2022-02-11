6e5f45a1dbdafd083d315fa22f386bae48b9fc52
Ypypy <kkkmit@mail.ru>
2022-02-11 15:59:12 +0300

    UpGrade this magic ball_1

--- app/src/main/java/com/example/magicball/MainActivity.kt
+++ app/src/main/java/com/example/magicball/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.magicball
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.example.magicball.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bindingClass: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+        }
+        return true
     }
 }
