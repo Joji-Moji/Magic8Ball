195fa0c13cfb3d22f9a2252fd4e0c8e4ffd58bf1
Ypypy <kkkmit@mail.ru>
2022-02-19 21:41:46 +0300

    UpGrade this magic ball_5

--- app/src/main/java/com/example/magicball/MainActivity.kt
+++ app/src/main/java/com/example/magicball/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.magicball
 
+import android.content.Context
+import android.os.Vibrator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.animation.AnimationUtils
@@ -12,32 +14,46 @@ open class MainActivity : AppCompatActivity() {
 
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bindingClass = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
 
-        //ball
-        bindingClass.imageView.setOnClickListener{
 
-            val animAnswer= AnimationUtils.loadAnimation(this, R.anim.anim_answer)
+
+        //ball
+        bindingClass.imageBall.setOnClickListener{
             bindingClass.answer.text = getArr()
-            bindingClass.answer.startAnimation(animAnswer)
+
+           val vibratorService = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+
+           val animBall= AnimationUtils.loadAnimation(this, R.anim.anim_ball)
+           val animAnswer= AnimationUtils.loadAnimation(this, R.anim.anim_answer)
+
+           vibratorService.vibrate(600)
+           bindingClass.imageBall.startAnimation(animBall)
+           bindingClass.answer.startAnimation(animAnswer)
         }
+
         //exit
         bindingClass.buttonExit.setOnClickListener {
             finish()
         }
+
         //language
         bindingClass.buttonLang.setOnClickListener {
 
         }
+
+
+
         //sound
-
-
         bindingClass.buttonSound.setOnClickListener{
         }
     }
+
+
 
 
     private fun getArr(): String{
@@ -48,6 +64,8 @@ open class MainActivity : AppCompatActivity() {
         val size = resources.getStringArray(R.array.arr).size - 1
         return(0..size).random()
     }
+
+
 
 
 }
